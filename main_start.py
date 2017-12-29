54d0b4402a7add18f04dd7a179a03911d7dc1cce
andyfrei <andy.frei1981@gmail.com>
2017-12-29 19:26:18 +0100

    bla

--- main_start.py
+++ main_start.py
@@ -1,1 +1,11 @@
-print ("hello world")
+#Library immer zuerst
+import numpy as np
+import cv2
+
+#dann code
+print('hello world')
+
+img = cv2.imread('dasBild.jpg', 0)
+cv2.imshow('title', img)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
